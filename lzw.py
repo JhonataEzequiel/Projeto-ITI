cb35d98b8caf622d8f0143084d1250cfd9d7ddcc
enacom <jhonata.ezequiel@enacom.com.br>
2023-09-25 14:32:50 -0300

    mode 3 added

--- lzw.py
+++ lzw.py
@@ -1,17 +1,17 @@
 import pickle
+from copy import deepcopy
 from typing import List
 
 
-def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_size: int, lru_quantity: int,
+def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_size: int, lru_lfu_quantity: int,
                  uses_of_str: dict, original_dict_size: int = 256):
     if mode == 0:
         return dictionary, dictionary_size - 1
     elif mode == 1:
         i = max_dict_size
-        inverted_keys = list(dictionary.keys())[::-1]
-        inverted_dict = {key: dictionary[key] for key in inverted_keys}
+        sorted_dict = dict(sorted(dictionary.items(), key=lambda _: _[1], reverse=True))
         items_to_remove = []
-        for dict_item in inverted_dict:
+        for dict_item in sorted_dict:
             items_to_remove.append(dict_item)
             i -= 1
             if i == original_dict_size:
@@ -20,19 +20,33 @@ def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_siz
             dictionary.pop(item)
         dictionary_size = i
     elif mode == 2:
-        uses_of_str = dict(sorted(uses_of_str.items(), reverse=True))
         uses_to_remove = []
         i = 1
         for use in uses_of_str:
             dictionary.pop(use)
             uses_to_remove.append(use)
             i += 1
-            if i > lru_quantity:
-                dictionary_size = dictionary_size - lru_quantity
+            if i > lru_lfu_quantity:
+                dictionary_size = dictionary_size - lru_lfu_quantity
                 break
         for use in uses_to_remove:
             uses_of_str.pop(use)
         return dictionary, dictionary_size, uses_of_str
+    elif mode == 3:
+        uses_of_str = dict(sorted(uses_of_str.items(), key=lambda _: _[1]))
+        uses_to_remove = []
+        i = 1
+        for use in uses_of_str:
+            dictionary.pop(use)
+            uses_to_remove.append(use)
+            i += 1
+            if i > lru_lfu_quantity:
+                dictionary_size = dictionary_size - lru_lfu_quantity
+                break
+        for use in uses_to_remove:
+            uses_of_str.pop(use)
+        return dictionary, dictionary_size, uses_of_str
+
     return dictionary, dictionary_size
 
 
@@ -53,20 +67,22 @@ def compress(_input, dictionary_size: int = 256, max_dict_size: int = 512, mode:
     original_dict_size = dictionary_size
 
     dictionary = {chr(i): i for i in range(dictionary_size)}
+    original_dict = deepcopy(dictionary)
     for c in _input:
         temp2 = temp + chr(c)
         if temp2 in dictionary.keys():
             temp = temp2
         else:
-            if temp2 in uses_of_str:
-                uses_of_str[temp2] += 1
-            else:
-                uses_of_str[temp2] = 1
+            if temp not in original_dict.keys():
+                if temp in uses_of_str.keys():
+                    uses_of_str[temp] += 1
+                else:
+                    uses_of_str[temp] = 1
             result.append(dictionary[temp])
             dictionary[temp2] = dictionary_size
             dictionary_size += 1
             if mode != 5 and dictionary_size > max_dict_size-1:
-                if mode == 2:
+                if mode in {2, 3}:
                     dictionary, dictionary_size, uses_of_str = set_max_dict(dictionary, mode, max_dict_size,
                                                                             dictionary_size, lru_quantity, uses_of_str,
                                                                             original_dict_size)
