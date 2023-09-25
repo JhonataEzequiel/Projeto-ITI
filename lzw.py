72d92d271cad234d752e1869a55ce69e3acabdb8
enacom <jhonata.ezequiel@enacom.com.br>
2023-09-25 15:52:59 -0300

    corrections added

--- lzw.py
+++ lzw.py
@@ -3,26 +3,19 @@ import sys
 from copy import deepcopy
 
 
-def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_size: int, lru_lfu_quantity: int,
+def set_max_dict(dictionary: dict, mode: int, dictionary_size: int, lru_lfu_quantity: int,
                  uses_of_str: dict, original_dict: dict, original_dict_size: int = 256):
-    if mode == 0:
-        return dictionary, dictionary_size
-    elif mode == 1:
-        i = max_dict_size
-        sorted_dict = dict(sorted(dictionary.items(), key=lambda _: _[1], reverse=True))
-        items_to_remove = []
-        for dict_item in sorted_dict:
-            items_to_remove.append(dict_item)
-            i -= 1
-            if i == original_dict_size:
-                break
-        for item in items_to_remove:
-            dictionary.pop(item)
-        dictionary_size = i
+    if mode in {1, 4}:
+        dictionary = deepcopy(original_dict)
+        dictionary_size = original_dict_size
+        if mode == 4:
+            return dictionary, dictionary_size, {}
     elif mode == 2:
         uses_to_remove = []
+        reversed_dict = list(dictionary.keys())[::-1]
+        reversed_dict = {key: dictionary[key] for key in reversed_dict}
         i = 1
-        for use in uses_of_str:
+        for use in reversed_dict:
             dictionary.pop(use)
             uses_to_remove.append(use)
             i += 1
@@ -30,7 +23,8 @@ def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_siz
                 dictionary_size = dictionary_size - lru_lfu_quantity
                 break
         for use in uses_to_remove:
-            uses_of_str.pop(use)
+            if use in uses_of_str:
+                uses_of_str.pop(use)
         return dictionary, dictionary_size, uses_of_str
     elif mode == 3:
         uses_of_str = dict(sorted(uses_of_str.items(), key=lambda _: _[1]))
@@ -46,9 +40,6 @@ def set_max_dict(dictionary: dict, mode: int, max_dict_size: int, dictionary_siz
         for use in uses_to_remove:
             uses_of_str.pop(use)
         return dictionary, dictionary_size, uses_of_str
-    elif mode == 4:
-        dictionary = original_dict
-        return dictionary, dictionary_size, {}
 
     return dictionary, dictionary_size
 
@@ -93,13 +84,12 @@ def compress(_input, dictionary_size: int = 256, max_dict_size: int = 512, mode:
                 rc = size_of_input / size_of_result
             if (mode not in {5, 4} and dictionary_size > max_dict_size) or (rc < min_rc and mode == 4):
                 if mode in {2, 3, 4}:
-                    dictionary, dictionary_size, uses_of_str = set_max_dict(dictionary, mode, max_dict_size,
-                                                                            dictionary_size, lru_quantity, uses_of_str,
-                                                                            original_dict, original_dict_size)
+                    dictionary, dictionary_size, uses_of_str = set_max_dict(dictionary, mode, dictionary_size,
+                                                                            lru_quantity, uses_of_str, original_dict,
+                                                                            original_dict_size)
                 else:
-                    dictionary, dictionary_size = set_max_dict(dictionary, mode, max_dict_size, dictionary_size,
-                                                               lru_quantity, uses_of_str, original_dict,
-                                                               original_dict_size)
+                    dictionary, dictionary_size = set_max_dict(dictionary, mode, dictionary_size, lru_quantity,
+                                                               uses_of_str, original_dict, original_dict_size)
             temp = f"{chr(c)}"
 
     if temp != "":
@@ -111,5 +101,5 @@ def compress(_input, dictionary_size: int = 256, max_dict_size: int = 512, mode:
 _input = open("dickens", "rb").read()
 _output = open("compressed_dickens", "wb")
 
-compressedFile = compress(_input, mode=4)
+compressedFile = compress(_input, mode=5)
 pickle.dump(compressedFile, _output)
